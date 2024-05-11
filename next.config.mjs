01f607d37e47a1691a18b75ce7a73881a0099967
coderhyy <1014873097@qq.com>
2024-05-11 11:55:26 +0800

    ci: 添加EditorConfig等配置

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    typedRoutes: true,
+  },
   webpack: (config) => {
     config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
